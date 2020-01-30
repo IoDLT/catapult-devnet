f7204e3a9c9fa34911c6677cd6fa3027444e1e99
Bader Youssef <ibnbassem@gmail.com>
2020-01-30 08:49:13 -0500

    update the update script

--- utils/update.sh
+++ utils/update.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-cp config.json ../config.json.BAK
+# backup config
+cp config.json /tmp/config.json.BAK
 # Reset docker images
 docker system prune -a
 # Reset repo to default
 git reset HEAD --hard
+
+# Make sure data is reset
+source reset_node.sh
+
 # Pull latest
 git pull
+
 # copy config back
-cp ../config.json.BAK config.json
+cp /tmp/config.json.BAK config.json
+
 python3 config.py
 echo Ready for starting, now use bash start.sh
 echo
